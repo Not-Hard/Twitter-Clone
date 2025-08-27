9411328f556b0b2cbe1266638414fdfa5931646a
[Not-Hard] <[juliopitno@gmail.com]>
2025-08-27 11:50:11 +0100

    feat: update user profile added

--- backend/routes/user_route.js
+++ backend/routes/user_route.js
@@ -1,13 +1,13 @@
 import express from 'express';
-import { getUserProfile, followUnfollowUser, getSuggestedUsers } from '../controllers/user_controller.js';
+import { getUserProfile, followUnfollowUser, getSuggestedUsers, updateUserProfile } from '../controllers/user_controller.js';
 import { protectRoute } from '../middleware/protectRoute.js';
 
 const router = express.Router();
 
 router.get('/profile/:username', protectRoute, getUserProfile);
 router.get('/suggested', protectRoute, getSuggestedUsers);
-router.get('/profile/follow/:id', protectRoute, followUnfollowUser);
-//router.get('/profile/update',protectRoute, updateUserProfile);
+router.post('/follow/:id', protectRoute, followUnfollowUser);
+router.post('/update', protectRoute, updateUserProfile); 
 
 
 
