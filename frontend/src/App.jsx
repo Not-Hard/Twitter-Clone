1cdc69ef5f7448899940a394d957952fdc835611
[Not-Hard] <[juliopitno@gmail.com]>
2025-09-04 19:23:38 +0100

    Update theme-controller

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,7 @@
 export default function App() {
   return (
     <>
-      <button className="btn btn-primary">Default</button>
-      <button className="btn btn-soft btn-primary">Primary</button>
-      <button className="btn btn-soft btn-secondary">Secondary</button>
-      <button className="btn btn-soft btn-accent">Accent</button>
-      <button className="btn btn-soft btn-info">Info</button>
-      <button className="btn btn-soft btn-success">Success</button>
-      <button className="btn btn-soft btn-warning">Warning</button>
-      <button className="btn btn-soft btn-error">Error</button>
+      <p>hello </p>
     </>
 
   );
