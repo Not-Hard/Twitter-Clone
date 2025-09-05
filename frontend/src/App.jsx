a7b4f2a93104e554e42f08d25507d896c6591e88
[Not-Hard] <[juliopitno@gmail.com]>
2025-09-05 19:30:59 +0100

    feat: UI Design Completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,30 @@
+import { Route, Routes} from "react-router-dom"
+
+import HomePage from "./pages/home/homePage";
+import  SignUpPage  from "./pages/authentication/signup/signupPage";
+import LoginPage from "./pages/authentication/login/loginPage";
+import NotificationPage from "./pages/notification/notificationPage";
+import ProfilePage from "./pages/profile/profilePage";
+
+import Sidebar from "./components/common/sideBar";
+import RightPanel from "./components/common/rightPanel";
+
+
+
 export default function App() {
   return (
-    <>
-      <p>hello </p>
-    </>
+    <div className='flex max-w-6xl mx-auto'>
+      {/* Common Component, it's not wrapped with Routes*/}
+      <Sidebar/>
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path='/signup' element={<SignUpPage />} />
+				<Route path='/login' element={<LoginPage />} />
+        <Route path='/notifications' element={<NotificationPage/>}/>
+        <Route path='/profile/:username' element={<ProfilePage/>}/>
+			</Routes>
+      <RightPanel/>
+		</div>
 
   );
 }
