b8873ae7243a6605aec26145ea25bebe371e7424
[Not-Hard] <[juliopitno@gmail.com]>
2025-09-15 14:08:06 +0100

    feat: create post  function added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,10 +5,19 @@ import "./index.css";
 import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
+// Define a default query function that will receive the query key
+const defaultQueryFn = async ({ queryKey }) => {
+  const { data } = await axios.get(
+    `https://jsonplaceholder.typicode.com${queryKey[0]}`,
+  )
+  return data
+};
+
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      refetchOnWindowFocus: false
+      refetchOnWindowFocus: false,
+      queryFn: defaultQueryFn,
     }
   }
 });
